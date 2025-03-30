e472693c453d417703cf2ae3ac6c2ee987e94c08
ngthnam24 <namnt240204@gmail.com>
2025-03-30 23:33:11 +0700

    Add files via upload

---
+++ nguyenthanhnam_2280602023_TUAN5/lab-03/api.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+from cipher.rsa import RSACipher
+
+app = Flask(__name__)
+
+#RSA CIPHER ALGORITHM
+rsa_cipher = RSACipher()
+
+@app.route('/api/rsa/generate_keys', methods=['GET'])
+def rsa_generate_keys():
+    rsa_cipher.generate_keys()
+    return jsonify({'message': 'Keys generated successfully'})
+
+@app.route("/api/rsa/encrypt", methods=["POST"])
+def rsa_encrypt():
+    data = request.json
+    message = data['message']
+    key_type = data['key_type']
+    private_key, public_key = rsa_cipher.load_keys()
+    if key_type == 'public':
+        key = public_key
+    elif key_type == 'private':
+        key = private_key
+    else:
+        return jsonify({'error': 'Invalid key type'})
+    encrypted_message = rsa_cipher.encrypt(message, key)
+    encrypted_hex = encrypted_message.hex()
+    return jsonify({'encrypted_message': encrypted_hex})
